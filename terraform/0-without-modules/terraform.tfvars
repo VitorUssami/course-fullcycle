c9dc6464e24dd1fc3eb80bba92bd76cc525d2b35
VitorUssami <vitor.ussami@gmail.com>
2023-02-09 22:00:35 -0300

    feat: add eks and node-group

--- terraform/0-without-modules/terraform.tfvars
+++ terraform/0-without-modules/terraform.tfvars
@@ -1,2 +1,6 @@
 prefix = "course-full-cycle"
 cluster_name = "course-full-cycle-cluster" 
+retention_days = 14
+desired_size = 2
+max_size = 4
+min_size = 2
--- terraform/0-without-modules/vpc.tf
+++ terraform/0-without-modules/vpc.tf
@@ -66,33 +66,3 @@ resource "aws_route_table_association" "new-rtb-association" {
     subnet_id = aws_subnet.subnets.*.id[count.index]
   
 }
-
-resource "aws_iam_role" "cluster" {
-    name = "${var.prefix}-${var.cluster_name}-role"
-    assume_role_policy = <<POLICY
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "eks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-            }
-        ]
-    }   
-    POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "cluster-AmazonEksVpcResourceController" {
-
-    role = aws_iam_role.cluster.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"  
-}
-
-resource "aws_iam_role_policy_attachment" "cluster-AmazonEksClusterPolicy" {
-
-    role = aws_iam_role.cluster.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"  
-}
